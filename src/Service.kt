6e712a4d1ee645a3badb7dc1d79b1b59fef8cca3
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-05-09 18:55:44 +0430

    Update the service to implement the HTTP protocol and remove the client

--- src/Service.kt
+++ src/Service.kt
@@ -1,77 +1,76 @@
-import java.io.DataInputStream
-import java.io.DataOutputStream
+import java.io.*
 import java.net.ServerSocket
 import java.net.Socket
 import java.time.Duration
 import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 private const val PORT = 8000
-private const val CONNECTION_END_FLAG = "END"
 private val startTime = Instant.now()
-private val clients = mutableMapOf<Int, MutableList<Pair<String, String>>>()
-private var clientsCount = 0
 
 fun main() {
     val serverSocket = ServerSocket(PORT)
     while (true) {
-        clientsCount++
         val socket = serverSocket.accept()
-        val client = Client(socket, clientsCount)
+        val client = Client(socket)
         val thread = Thread(client)
         thread.start()
     }
 }
 
-class Client(private val socket: Socket, private val clientNumber: Int) : Runnable {
+class Client(socket: Socket) : Runnable {
 
-    private val input = DataInputStream(socket.getInputStream())
-    private val output = DataOutputStream(socket.getOutputStream())
+    private val input = BufferedReader(InputStreamReader(socket.getInputStream()))
+    private val output = BufferedWriter(OutputStreamWriter(socket.getOutputStream()))
 
     override fun run() {
-        clients[clientNumber] = mutableListOf()
-
-        output.writeUTF(""" 
-            U -> Get uptime (send U)
-            G -> Get current data (send G)
-            S -> Store a data pair (send S followed by a pair of values; separate by space)
-            D -> Delete a data pair (send D followed by the first value in the desired pair)
-        """)
-
-        var message = input.readUTF()
-        while (message != CONNECTION_END_FLAG) {
-            when (message.first().toUpperCase()) {
-                'U' -> outputUptime()
-                'G' -> outputData()
-                'S' -> storeData(message)
-                'D' -> deleteData(message)
-                else -> output.writeUTF("Bad command!")
-            }
-            message = input.readUTF()
-        }
-
-        socket.close()
+        val requestLine = input.readLine()
+        val path = requestLine.split(" ")[1]
+        dispatch(path)
     }
 
-    private fun outputUptime() {
+    private fun dispatch(path: String) = when (path) {
+        "/" -> index()
+        "/uptime" -> uptime()
+        "/date" -> date()
+        else -> error()
+    }
+
+    private fun index() {
+        val payload = """
+            / -> index page
+            /uptime -> Get service uptime
+            /date -> Get current date
+        """
+        writeResponse(payload)
+    }
+
+    private fun uptime() {
         val duration = Duration.between(startTime, Instant.now())
-        output.writeUTF("${duration.seconds}s")
+        val payload = "${duration.seconds}s"
+        writeResponse(payload)
     }
 
-    private fun outputData() {
-        val data = clients.getValue(clientNumber)
-        output.writeUTF("$data")
+    private fun date() {
+        val payload = "${LocalDate.now()}"
+        writeResponse(payload)
     }
 
-    private fun storeData(message: String) {
-        val (first, second) = message.removeRange(0, 2).split(' ')
-        val pair = Pair(first, second)
-        clients.getValue(clientNumber).add(pair)
-        output.writeUTF("Stored $pair")
+    private fun error() {
+        val payload = "Error!"
+        writeResponse(payload)
     }
 
-    private fun deleteData(message: String) {
-        val key = message.removeRange(0, 2)
-        clients.getValue(clientNumber).removeIf { it.first == key }
-        output.writeUTF("Deleted the pair with key $key")
+    private fun writeResponse(payload: String) {
+        output.appendln("HTTP/1.1 200 OK")
+        output.appendln("Date: ${LocalDateTime.now()}")
+        output.appendln("Content-type: text/plain")
+        output.appendln("Content-length: ${payload.toByteArray().size}")
+
+        output.newLine() // blank line between headers and content
+
+        output.write(payload)
+        output.flush()
     }
 }
